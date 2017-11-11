8b72181db2b1b537a8fb5a60c53377767c540fe5
Daniil Nikitas <daniil_nikitas@mail.ru>
2017-11-11 16:15:54 +0600

    menu

--- js/main.js
+++ js/main.js
@@ -1,10 +1,29 @@
 (function() {
-	const link = $('.hamburger-menu-link');
-	let munu = $('.mobile__nav ')
-	active = 'active';
+	const link = $('.hamburger-menu-link'),
+		closeLink = $(".hamburger-menu-link--active");
+
+	let menu = $('.mobile__nav'),
+		active = 'active';
+
+
 	link.click(function(event) {
-		munu.addClass(active);
+		menu.addClass(active);
 	});
 
-});
+	closeLink.click(function() {
+		menu.removeClass(active); // удаление класса
+	});
+$(document).keyup(function(e) {
 
+		// условие, если нажатие было на ESC
+		if(e.which == 27) {
+			menu.removeClass(active); // удаление класса
+		}
+	});
+$(document).mouseup(function(e) {
+		// если не на область блока меню
+		if(!menu.has(e.target).length) {
+			menu.removeClass(active); // удаление класса
+		}
+	});
+})();
