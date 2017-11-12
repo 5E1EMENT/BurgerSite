11047e65423f99574e2463f85ccab431433d126b
Daniil Nikitas <daniil_nikitas@mail.ru>
2017-11-12 12:11:21 +0600

    mobile-menu-done

--- js/main.js
+++ js/main.js
@@ -1,29 +1,7 @@
-(function() {
-	const link = $('.hamburger-menu-link'),
-		closeLink = $(".hamburger-menu-link active");
-
-	let menu = $('.mobile__nav'),
-		active = 'active';
-
-
-	link.click(function(event) {
-		menu.addClass(active);
+ $(document).ready(function(){
+	$('.hamburger-menu-link').click(function () {
+		$('.mobile__nav').toggleClass('active');
+		});
 	});
 
-	closeLink.click(function() {
-		menu.removeClass(active); // удаление класса
-	});
-$(document).keyup(function(e) {
 
-		// условие, если нажатие было на  ESC
-		if(e.which == 27) {
-			menu.removeClass(active); // удаление класса
-		}
-	});
-$(document).mouseup(function(e) {
-		// если не на область блока меню
-		if(!menu.has(e.target).length) {
-			menu.removeClass(active); // удаление класса
-		}
-	});
-})();
