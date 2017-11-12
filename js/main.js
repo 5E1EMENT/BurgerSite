311540d262155f223bb6b13bbef4dafd918fd94e
Daniil Nikitas <daniil_nikitas@mail.ru>
2017-11-12 20:42:09 +0600

    fixes-hide-menu-click

--- js/main.js
+++ js/main.js
@@ -24,11 +24,11 @@
 
 	// при нажатии на мышку
 	// при нажатии на мышку
-	$(document).mouseup(function(e) {
-		// если не на область блока меню
-		var menuBlock = $('.mobile__nav');
-		var active = $('.active');
-		if(!menuBlock.has(e.target).length) {
-			menuBlock.removeClass(active); // удаление класса
-		}
-	});
+	
+
+	 $(document).mouseup(function(e) {
+            var $target = $(e.target);
+            if ($target.closest(".mobile__item-hidden").length === 0) {
+                $(".mobile__nav").removeClass("active");
+            }
+        });
