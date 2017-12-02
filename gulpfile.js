5b4db42dc6bc1caaf256927a6f72fd1c8ff3d912
Daniil Nikitas <daniil_nikitas@mail.ru>
2017-12-02 19:56:00 +0600

    deleted unnescessary files

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,17 @@
 var gulp = require('gulp');
-gulp.task('default', function(callback) {
-console.log('Hello world');
-callback();
+var concatCss = require('gulp-concat-css');
+let cleanCSS = require('gulp-clean-css');
+let rename = require('gulp-rename')
+let notify = require('gulp-notify')
+
+gulp.task('default', function () {
+  return gulp.src('css/*.css')
+    .pipe(concatCss("bundle.css"))
+    .pipe(cleanCSS(""))
+    .pipe(rename("bundle.min.css"))
+    .pipe(gulp.dest('css/'))
+    .pipe(notify('Done!'));
+});
+gulp.task('watch', function(){
+	gulp.watch('css/*.css', ['default'])
 });
